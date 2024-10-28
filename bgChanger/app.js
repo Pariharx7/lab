b340fb1adc40a9250ea619134aa85ff85802088f
Pariharx7 <parihar.peace@gmail.com>
2024-10-28 22:30:11 +0530

    Added audio folder

--- bgChanger/app.js
+++ bgChanger/app.js
@@ -26,6 +26,6 @@ function generateRandomColor(){
 let red = Math.floor(Math.random() * 256);
 let green = Math.floor(Math.random() * 256);
 let blue = Math.floor(Math.random() * 256);
-                body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    body.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
 }
