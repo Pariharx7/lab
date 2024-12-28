35c92cf43c8d63c0853de85d0d8da3be4e15fc56
Pariharx7 <parihar.peace@gmail.com>
2024-12-28 19:30:19 +0530

    Added app.js

--- page/app.js
+++ page/app.js
@@ -2,11 +2,13 @@ const showButton = document.getElementById("show");
 const closeButton = document.getElementById("close");
 const navBar = document.querySelector("nav");
 showButton.addEventListener('click', () => {
+    navBar.style.transition = "right 7s ease-in";
     navBar.style.opacity = "1";
     navBar.style.width = "300px";
 })
 
 closeButton.addEventListener('click', () => {
+    navBar.style.transition = "left 3s ease-in";
     navBar.style.opacity = "0";
     navBar.style.width = "0";
 })
