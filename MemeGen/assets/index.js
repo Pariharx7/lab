784b5e7360c68bb9c00f57624d35cdd07aa61c57
Pariharx7 <parihar.peace@gmail.com>
2025-05-05 22:24:10 +0530

    Updated index.js

--- MemeGen/assets/index.js
+++ MemeGen/assets/index.js
@@ -18,3 +18,6 @@
  * scheduler.production.js
  *
  * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * *
+    * This source code is licensed under the MIT license found in the
+    * LICENSE file in the root directory of this source tree.
