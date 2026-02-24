b32f92d6f18e1b79588394fe65eb19938cc43ac8
Pariharx7 <parihar.peace@gmail.com>
2026-02-24 18:00:27 +0530

    Updated script.js

--- cars/script.js
+++ cars/script.js
@@ -1,36 +1,57 @@
 const refreshButton = document.getElementById("refresh-btn");
 const pictureBoxes = document.querySelectorAll(".picture-box");
-const image1 = document.getElementById("im1");
-// console.log(pictureBoxes);
+const imageCollection = document.querySelectorAll(".picturebox-img")
+console.log(imageCollection);
 
 refreshButton.addEventListener("click", () => {
     alert("Clicked");
 })
 
-//next thing to do is to find an API that provides images of cars// work on pixabay api
-
-const API_KEY = '54751795-c55b4d219897c093d0f1db0c0';
-const query = 'car';
 
 async function getCarPhoto() {
-    
+
     const response = await fetch(`https://pixabay.com/api/?key=54751795-c55b4d219897c093d0f1db0c0&q=cars&image_type=photo`);
     const data = await response.json();
-    console.log(data);
 
-    let images = data.hits[0].previewURL;
-    console.log("images url ",images)
-    console.log(data.hits[0].pageURL);
-    let urlx = data.hits[0].pageURL;
+    const randomNum = Math.floor(Math.random() * data.hits.length);
+    const images = data.hits[randomNum].previewURL;
+
+    return images
 }
 
-let image = getCarPhoto();
+
+function getRandomURL(url= ""){
+    getCarPhoto().then(ul => {
+        url = ul;
+    })
+    return url;
+}
+
+console.log("Line 30 getRandom => ",getRandomURL());
+
 
 window.addEventListener("load", () => {
-    image1.setAttribute("src","https://cdn.pixabay.com/photo/2024/07/13/07/40/cars-8891625_150.jpg");
+    let carUrl = "";
+    getCarPhoto().then(url => {
+        carUrl = url;
+        console.log("var updated", carUrl);
+    })
+    // image1.setAttribute("src", `${carUrl}`);
 })
 
 // we have got two ways to do this shit
 // one we can go the way it is 
 // or we can create a img tag push it into picture-box 
 // and for other stuff we need to iterate and push that shit in the box
+
+window.addEventListener("load", () => {
+    let carUrl = "";
+    getCarPhoto().then(url => {
+        carUrl = url;
+        console.log("var updated", carUrl);
+    })
+    imageCollection.forEach((img) => {
+        img.setAttribute("src", `${carUrl}`);
+        console.log("This takes time")
+    })
+})
