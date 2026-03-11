191d34fbdf4825803c2a94d00e3fd3869f1dca55
Pariharx7 <parihar.peace@gmail.com>
2026-03-11 17:02:21 +0530

    updated app.js

--- flappyBird/assets/app.js
+++ flappyBird/assets/app.js
@@ -1,9 +1,20 @@
+const canvas = document.querySelector("#gameCanvas");
+let ctx = canvas.getContext("2d");
 let bird = document.getElementById("bird");
+
+setInterval(() => {
+    ctx.clearRect(0, 0, 1400, 600)
+    ctx.beginPath();
+    ctx.rect(1, genRan(), 20, 20);
+    ctx.fill();
+}, 1000)
+
+
 function genRan(){
     return random = Math.floor(Math.random() * 482)
 }
-console.log(genRan())
-setInterval(() => {
-    console.log(bird.style.top);
-    bird.style.top = `${genRan()}px`;
-}, 1000)
+// console.log(genRan())
+// setInterval(() => {
+// //     console.log(bird.style.top);
+// //     bird.style.top = `${genRan()}px`;
+// // }, 1000)
