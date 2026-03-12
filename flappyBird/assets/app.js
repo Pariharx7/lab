6ae68aa139b7860748f1d10e53cefa25b0b58d4e
Pariharx7 <parihar.peace@gmail.com>
2026-03-12 17:15:13 +0530

    Updated app.js

--- flappyBird/assets/app.js
+++ flappyBird/assets/app.js
@@ -1,17 +1,22 @@
-const canvas = document.querySelector("#gameCanvas");
-let ctx = canvas.getContext("2d");
-let bird = document.getElementById("bird");
+async function main() {
+    const canvas = document.querySelector("#gameCanvas");
+    let ctx = canvas.getContext("2d");
+    let bird = document.getElementById("bird");
+    let y = 0;
+    setInterval(() => {
+        y = y + 1;
+        ctx.clearRect(0, 0, 1400, 600)
+        ctx.beginPath();
+        ctx.rect(1, y, 20, 20);
+        ctx.fill();
+    }, 1000)
+    await new Promise((r) => setTimeout(r, 0.33333333 * 1000));
 
-setInterval(() => {
-    ctx.clearRect(0, 0, 1400, 600)
-    ctx.beginPath();
-    ctx.rect(1, genRan(), 20, 20);
-    ctx.fill();
-}, 1000)
+}
+main()
 
-
-function genRan(){
-    return random = Math.floor(Math.random() * 482)
+function genRan() {
+    return random = Math.floor(Math.random() * 100)
 }
 // console.log(genRan())
 // setInterval(() => {
