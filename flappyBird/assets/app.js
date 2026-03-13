a3a663b3000c2031507e8fd8d63e7ef5f871d849
Pariharx7 <parihar.peace@gmail.com>
2026-03-13 18:39:18 +0530

    Updated app.js

--- flappyBird/assets/app.js
+++ flappyBird/assets/app.js
@@ -1,31 +1,14 @@
-// async function main() {
-//     const canvas = document.querySelector("#gameCanvas");
-//     let ctx = canvas.getContext("2d");
-//     let y = 0;
-//     setInterval(() => {
-//         y = y + 1;
-//         ctx.clearRect(0, 0, 1400, 600)
-//         ctx.beginPath();
-//         ctx.rect(1, y, 20, 20);
-//         ctx.fill();
-//     }, 1000)
-//     await new Promise((r) => setTimeout(r, 0.33333333 * 1000));
-
-// }
-// main()
-
-
-//trying diff approach
-
 const canvas = document.querySelector("#gameCanvas");
 let ctx = canvas.getContext("2d");
+let img = new Image();
+img.src = "./assets/images/bxrd.png";
 
 let birdY = 200;
 let birdX = 10;
 let birdSize = 30;
 let velocity = 0;
-let gravity = 0.5;
-let jump = -8;
+let gravity = 0.8;
+let jump = -7;
 
 
 
@@ -45,7 +28,8 @@ function update(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = "#FF5733";
-    ctx.fillRect(birdX, birdY, birdSize, birdSize);
+    // ctx.fillRect(birdX, birdY, birdSize, birdSize);
+    ctx.drawImage(img,birdX, birdY, 50, 50);
 
     requestAnimationFrame(update);
 }
